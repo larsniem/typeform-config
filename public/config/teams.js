85aaff527015d99c46aa4dd7e68e3d05be110bd7
Lars <niemczik@dik.tu-darmstadt.de>
2022-02-04 17:42:19 +0100

    No Config Test Script Execute

--- public/config/teams.js
+++ public/config/teams.js
@@ -1,3 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("submit-12345678").addEventListener("click", changeElementContent)
+})
+
 function changeElementContent() {
     console.debug("Change element content")
     var element = window.document.getElementById("test-12345678");
