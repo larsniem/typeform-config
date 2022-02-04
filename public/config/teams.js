b1279d5b21c4b746d3c0309718b55e7f9c4b2cd3
Lars <niemczik@dik.tu-darmstadt.de>
2022-02-04 16:19:48 +0100

    No Config Test Script Execute

---
+++ public/config/teams.js
@@ -0,0 +1,5 @@
+function changeElementContent() {
+    console.debug("Change element content")
+    var element = window.document.getElementById("test-12345678");
+    element.innerText = "Nur ein Test.";
+}
